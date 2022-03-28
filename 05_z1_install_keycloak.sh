03837e18a833ba73f446c18ae6886a87f66835bf
Jugwan Eom <zugwan@gmail.com>
2022-03-28 05:59:07 +0000

    make use keycloak-operator

--- 05_z1_install_keycloak.sh
+++ 05_z1_install_keycloak.sh
@@ -1,36 +1,22 @@
 #!/bin/bash
 
+#!/bin/bash
+
 set -e
 
 source common.sh
 
-if [ -z "$1" ]
-  then
-    echo "usage: $0 <helm value file for overriding>"
-    exit 1
-fi
-
-HELM_VALUE_FILE=$1
 CLUSTER_NAME=$(kubectl get cluster -o=jsonpath='{.items[0].metadata.name}')
 
 export KUBECONFIG=kubeconfig_$CLUSTER_NAME
 
-print_msg "Configure PostgreSQL DB for keycloak..."
-PSQL_CMD="kubectl exec -n decapod-db postgresql-postgresql-0 -- bash -c \"PGPASSWORD=tacopassword /opt/bitnami/postgresql/bin/psql -U postgres -c"
-
-eval ${PSQL_CMD} "\\\"CREATE DATABASE keycloak;\\\"\""
-eval ${PSQL_CMD} "\\\"CREATE USER keycloak WITH ENCRYPTED PASSWORD 'keycloak';\\\"\""
-eval ${PSQL_CMD} "\\\"GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;\\\"\""
-print_msg "... done"
-
 print_msg "Installing Keycloak..."
-kubectl create ns keycloak
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install keycloak bitnami/keycloak -n keycloak -f $HELM_VALUE_FILE
+
+argo submit --from wftmpl/install-admin-tools -p app_prefix=tks-admin -n argo
 
 for ns in keycloak; do
-	for po in $(kubectl get po -n $ns -o jsonpath='{.items[*].metadata.name}');do
-		kubectl wait --for=condition=Ready -n $ns --timeout=180s po/$po
-	done
+        for po in $(kubectl get po -n $ns -o jsonpath='{.items[*].metadata.name}');do
+                kubectl wait --for=condition=Ready -n $ns --timeout=180s po/$po
+        done
 done
 print_msg "... done"
