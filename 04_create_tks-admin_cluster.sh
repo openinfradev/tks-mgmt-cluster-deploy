70c40274882767f88cfb1afa3953ea84445d696f
Jugwan Eom <zugwan@gmail.com>
2022-03-28 05:18:37 +0000

    fix TKS admin cluster creation stage

--- 04_create_tks-admin_cluster.sh
+++ 04_create_tks-admin_cluster.sh
@@ -14,10 +14,6 @@ fi
 ASSET_DIR=$1
 HELM_VALUE_FILE=$2
 HELM_VALUE_K8S_ADDONS="--set cni.calico.enabled=true"
-YQ_VERSION=$(ls $1/yq)
-
-sudo cp $1/yq/$YQ_VERSION/yq_linux_amd64 /usr/local/bin/yq
-sudo chmod +x /usr/local/bin/yq
 
 print_msg "Creating TKS Admin Cluster via Cluster API"
 
@@ -26,21 +22,6 @@ create_capa_cluster () {
 	helm install tks-admin $CHART_DIR -f $HELM_VALUE_FILE
 
 	CLUSTER_NAME=$(kubectl get cluster -o=jsonpath='{.items[0].metadata.name}')
-
-	echo "Update subnets for the machinepool..."
-
-	while true
-	do
-		[ $(kubectl get awscluster $CLUSTER_NAME -o yaml  |  yq e '.spec.networkSpec.subnets.[] | select(.isPublic != true) | [ .id ]' -  | wc -l) !=  $NUMBER_OF_AZS_IN_THE_REGION ] && continue
-
-		break
-	done
-
-	kubectl get awscluster $CLUSTER_NAME -o yaml  |  yq e '.spec.networkSpec.subnets.[] | select(.isPublic != true) | [ { "id": .id } ]' - > priv_subnets
-	cat priv_subnets
-	NEW_HELM_VALUE_FILE="${HELM_VALUE_FILE%.*}-mp_subnets.vo"
-	yq ea 'select(fileIndex==0).machinePool.subnets = select(fileIndex==1) | select(fileIndex==0)' $HELM_VALUE_FILE priv_subnets > $NEW_HELM_VALUE_FILE
-	helm upgrade tks-admin $CHART_DIR -f $NEW_HELM_VALUE_FILE
 }
 
 create_capo_cluster () {
@@ -51,7 +32,6 @@ create_capo_cluster () {
 case $CAPI_INFRA_PROVIDER in
         "aws")
                 create_capa_cluster
-                HELM_VALUE_K8S_ADDONS+=" --set csi.aws-ebs.enabled=true"
                 ;;
 
         "openstack")
@@ -93,7 +73,8 @@ do
 
   case $CAPI_INFRA_PROVIDER in
 	  "aws")
-		  kubectl wait --for=condition=Ready awsmachinepool/$CLUSTER_NAME-mp-0 || continue
+		  MP_NAME=$(kubectl get mp -ojsonpath={.items[0].metadata.name})
+		  kubectl wait --for=condition=Ready awsmachinepool/$MP_NAME || continue
 		  ;;
 
 	  "openstack")
@@ -113,7 +94,8 @@ clusterctl get kubeconfig $CLUSTER_NAME > kubeconfig_$CLUSTER_NAME
 chmod 600 kubeconfig_$CLUSTER_NAME
 
 print_msg  "Installing kubernetes addons for network and stroage"
-helm install --kubeconfig kubeconfig_$CLUSTER_NAME k8s-addons $ASSET_DIR//taco-helm/kubernetes-addons $HELM_VALUE_K8S_ADDONS
+helm install --kubeconfig kubeconfig_$CLUSTER_NAME k8s-addons $ASSET_DIR/taco-helm/kubernetes-addons $HELM_VALUE_K8S_ADDONS
+helm install --kubeconfig kubeconfig_$CLUSTER_NAME aws-ebs-csi-driver --namespace kube-system $ASSET_DIR/aws-ebs-csi-driver/aws-ebs-csi-driver
 
 for node in $(kubectl get no --kubeconfig kubeconfig_$CLUSTER_NAME -o jsonpath='{.items[*].metadata.name}');do
 	kubectl wait --kubeconfig kubeconfig_$CLUSTER_NAME --for=condition=Ready no/$node
