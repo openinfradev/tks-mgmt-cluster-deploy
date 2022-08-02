801fe5ad32f2f7e4f4ed7fc10640b87e86689944
Seungkyu Ahn <seungkyua@gmail.com>
2022-08-02 20:03:21 +0900

    Add install gitea script and gitea app.ini.example

---
+++ 02_z2_install_gitea.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -ex
+
+GROUP=$(groups $USER | awk '{print $1}')
+
+curl -Lo gitea https://dl.gitea.io/gitea/1.17.0/gitea-1.17.0-linux-amd64
+chmod +x gitea
+
+sudo apt install -y sqlite
+
+sudo mkdir -p /var/lib/gitea/{custom,data,log}
+sudo chown -R $USER:$GROUP /var/lib/gitea/
+sudo chmod -R 750 /var/lib/gitea/
+sudo mkdir -p /etc/gitea
+sudo chown root:$GROUP /etc/gitea
+sudo chmod 770 /etc/gitea
+
+export GITEA_WORK_DIR=/var/lib/gitea/
+nohup ./gitea web -c /etc/gitea/app.ini 2>&1 1>gitea.log &
