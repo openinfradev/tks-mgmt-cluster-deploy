cdaf6b1958ad4e050eb9f0415114fa1315a1cffc
Taekyu <ktkfree@naver.com>
2022-11-18 17:52:36 +0900

    bugfix. replace "docker image load" logic

--- 02_z0_upload_container_images.sh
+++ 02_z0_upload_container_images.sh
@@ -10,20 +10,19 @@ then
 fi
 
 if [ $(sudo docker images | wc -l) -ne 0 ]; then
-	log_warn "docker images are already exist"
-	gum confirm "Are you sure you want to include the images and proceed?" || exit 1
+        log_warn "docker images are already exist"
+        gum confirm "Are you sure you want to include the images and proceed?" || exit 1
 fi
 
 ASSETS_DIR=$1
 REGISTRY=$2
+cd $ASSETS_DIR/decapod-image
 
-for i in `ls ${ASSETS_DIR}/decapod-image`
-do
-  echo sudo docker load -i $i
-done 
+ls -1 *.gz | xargs --no-run-if-empty -L 1 docker load -i
 
-for i in  `sudo docker images | grep -v ${REGISTRY} | awk '{print $1":"$2}'  `
+# kube-webhook-certgen
+for i in  `docker images | grep -v ${REGISTRY} | grep -v REPOSITORY | grep -v kube-webhook-certgen | grep -v '<none>' | awk '{print $1":"$2}' `
 do
-  echo sudo docker tag $i ${REGISTRY}/"${i##*/}"
-  echo sudo docker push ${REGISTRY}/"${i##*/}"
+  docker tag $i ${REGISTRY}/"${i##*/}"
+  docker push ${REGISTRY}/"${i##*/}"
 done
