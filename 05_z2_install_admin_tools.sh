c3bc1f7ab74504e38cb1074eac2948f0564b55ed
taekyu.kang <ktkfree@naver.com>
2023-10-26 10:16:53 +0900

    bugfix. add workflow paramter db_common_password to tks-admin-tools

--- 05_z2_install_admin_tools.sh
+++ 05_z2_install_admin_tools.sh
@@ -11,6 +11,6 @@ export KUBECONFIG=~/.kube/config
 
 log_info "Calling workflow to install admin-tools(keycloak, harbor, etc)..."
 
-argo submit --from wftmpl/tks-install-admin-tools -n argo -p install_nginx=$INSTALL_NGINX_INGRESS -p manifest_repo_url="${GIT_SVC_BASE_URL}/${GIT_SVC_USERNAME}/${CLUSTER_NAME}-manifests" -p site_name=${CLUSTER_NAME} --watch
+argo submit --from wftmpl/tks-install-admin-tools -n argo -p install_nginx=$INSTALL_NGINX_INGRESS -p manifest_repo_url="${GIT_SVC_BASE_URL}/${GIT_SVC_USERNAME}/${CLUSTER_NAME}-manifests" -p site_name=${CLUSTER_NAME} -p db_common_password=$DATABASE_PASSWORD --watch
 
 log_info "...Done"
